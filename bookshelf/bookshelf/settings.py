b104c0091a31b73a875ee51954544f79374a1f59
never-sec <pasha20060316@gmail.com>
2025-08-30 14:29:07 +0300

    change STATICFILES_DIRS

--- bookshelf/bookshelf/settings.py
+++ bookshelf/bookshelf/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / "main" / "static",
 ]
 
 # Default primary key field type
