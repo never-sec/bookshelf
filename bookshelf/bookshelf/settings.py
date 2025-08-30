e2de3e26f3b5dc7c5a8be742ce4ac2124e04dd3c
never-sec <pasha20060316@gmail.com>
2025-08-30 13:31:21 +0300

    init

--- bookshelf/bookshelf/settings.py
+++ bookshelf/bookshelf/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
     '/var/www/static/',
