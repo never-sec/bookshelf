3a3fc626dc17f588ab95c4f46c4b7b8dbfaa12e6
never-sec <pasha20060316@gmail.com>
2025-08-30 14:43:25 +0300

    wsgi

--- bookshelf/bookshelf/wsgi.py
+++ bookshelf/bookshelf/wsgi.py
@@ -1,16 +1,15 @@
-"""
-WSGI config for bookshelf project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+
+# Добавьте ваш путь к проекту в Python path
+# Вам нужно, чтобы этот путь вел к папке, где находится manage.py
+path = '/home/studip/bookshelf/bookshelf'
+if path not in sys.path:
+    sys.path.append(path)
 
 from django.core.wsgi import get_wsgi_application
 
+# Укажите Django, где найти файл настроек
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bookshelf.settings')
 
 application = get_wsgi_application()
